0500c678f98b94b052dcfb483e25b4965ccba546
demo <praveen98845kumar@gmail.com>
2026-02-10 12:18:15 +0530

    Initial commit

---
+++ src/Basic/IterativeStatements.java
@@ -0,0 +1,23 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class IterativeStatements {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entern how many time :");
+        int num = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("enter the word :");
+        String word = sc.nextLine();
+
+
+
+        for (int i=1; i <= num;i++){
+            System.out.println(word);
+        }
+        sc.close();
+    }
+}
